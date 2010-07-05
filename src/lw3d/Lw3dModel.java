ad5afbb9e2a11de544cef7f30533da12ff0a9107
betelgeuze <betelge@gmail.com>
2010-07-05 22:13:04 +0000

    Move level initialization code to the client.

--- src/lw3d/Lw3dModel.java
+++ src/lw3d/Lw3dModel.java
@@ -49,9 +49,9 @@ import lw3d.utils.TextureLoader;
 
 public class Lw3dModel {
 	
-	private List<RenderPass> renderPasses = new ArrayList<RenderPass>();
+	protected List<RenderPass> renderPasses = new ArrayList<RenderPass>();
 	
-	final private Set<Node> simulatedNodes = new HashSet<Node>();
+	protected final Set<Node> simulatedNodes = new HashSet<Node>();
 	
 	private LinkedHashSet<Integer> keys = new LinkedHashSet<Integer>();
 	
@@ -62,120 +62,14 @@ public class Lw3dModel {
 	// Used to redirect drawing to an applet area
 	final private Canvas displayParent;
 	
-	private CameraNode cameraNode;
+	protected CameraNode cameraNode;
 	
 	public Lw3dModel() {
 		this(null);
 	}
 
 	public Lw3dModel(Canvas displayParent) {
-		this.displayParent = displayParent;
-		
-		
-		InputStream is = getClass().getResourceAsStream("/untitled.obj");
-		if(is == null)
-			System.out.println("Model cant't load geometry: " + "/untitled.obj");
-		else {
-			System.out.println("Model loads ok");
-		}
-
-		Geometry cubeMesh = GeometryLoader.loadObj("/untitled.obj");
-
-		Set<Shader> shaders = new HashSet<Shader>();
-		Set<Shader> fboShaders = new HashSet<Shader>();
-		
-		try {
-			shaders
-					.add(new Shader(
-							Shader.Type.VERTEX,
-							StringLoader.loadString("/default.vertex")));
-
-			shaders
-					.add(new Shader(
-							Shader.Type.FRAGMENT,
-							StringLoader.loadString("/default.fragment")));
-			
-			fboShaders
-				.add(new Shader(
-					Shader.Type.VERTEX,
-					StringLoader.loadString("/direct.vertex")));
-			fboShaders
-				.add(new Shader(
-					Shader.Type.FRAGMENT,
-					StringLoader.loadString("/direct.fragment")));
-
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		ShaderProgram shaderProgram = new ShaderProgram(shaders);
-		ShaderProgram fboShaderProgram = new ShaderProgram(fboShaders);
-		Material defaultMaterial = new Material(shaderProgram);
-		Material fboMaterial = new Material(fboShaderProgram);
-		
-		Node rootNode = new Node();
-		GeometryNode[] cubes = new GeometryNode[1-1];
-		
-		for(int i = 0; i < cubes.length; i++) {
-			cubes[i] = new GeometryNode(cubeMesh, defaultMaterial);
-			cubes[i].getTransform().getPosition().z = -100 * (float)Math.random() - 5;
-			cubes[i].getTransform().getPosition().x = cubes[i].getTransform().getPosition().z * (0.5f-(float)Math.random());
-			cubes[i].getTransform().getPosition().y = cubes[i].getTransform().getPosition().z * (0.5f-(float)Math.random());
-			
-			rootNode.attach(cubes[i]);
-		}
-		Uniform[] uniforms = new Uniform[1];
-		uniforms[0] = new Uniform("col2", 0f, 1f, 0f, 1f);
-		defaultMaterial.setUniforms(uniforms);
-		
-		Texture texture = null;
-		try {
-			texture = TextureLoader.loadTexture("/test.png");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		// FBO texture
-		Texture fboTexture = new Texture(null, TextureType.TEXTURE_2D,
-				Display.getDisplayMode().getWidth(), Display.getDisplayMode().getHeight(),
-				TexelType.UBYTE, Format.GL_RGBA8, Filter.LINEAR, WrapMode.CLAMP);
-		
-		RenderBuffer depthBuffer = new RenderBuffer(Format.GL_DEPTH_COMPONENT,
-				Display.getDisplayMode().getWidth(), Display.getDisplayMode().getHeight());
-		
-		FBO myFBO = new FBO(fboTexture, depthBuffer,
-				Display.getDisplayMode().getWidth(), Display.getDisplayMode().getHeight());
-		
-		defaultMaterial.addTexture("texture0", texture);
-		fboMaterial.addTexture("source", fboTexture);
-		simulatedNodes.add(rootNode);
-		cameraNode = new CameraNode();
-		rootNode.attach(cameraNode);
-		//cameraNode.getTransform().getPosition().z = -1f;
-		
-		// Create render passes
-		renderPasses.add(new SceneRenderPass(rootNode, cameraNode/*, myFBO*/));
-		//renderPasses.add(new QuadRenderPass(fboMaterial));
-		//renderPasses.add(new BloomPass(fboMaterial.getTextures().get("source")));
-		
-		MovableGeometryNode cube = new MovableGeometryNode(cubeMesh, defaultMaterial);
-		
-		rootNode.attach(cube);
-		
-		rootNode.attach(new Light(new Transform(new Vector3f(3f,3f,-15f), new Quaternion())));
-
-		cube.getTransform().getPosition().z = -15f;
-		cube.getTransform().getPosition().x = 1f;
-		//cube.getTransform().getPosition().y = -1f;
-		
-		/*cube.getTransform().getRotation().fromAngleAxis(
-				(float)Math.PI/2*3, new Vector3f(1f, 2f, 1f));*/
-
-		cube.getMovement().getPosition().x = -0.01f;
-		cube.getMovement().getRotation().fromAngleNormalAxis(0.03f, Vector3f.UNIT_Z);
-		
+		this.displayParent = displayParent;		
 	}
 
 	public List<RenderPass> getRenderPasses() {
